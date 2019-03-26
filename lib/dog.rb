f6148d97e45b03f0e4060fe6182f3af224ec8cea
Tony Baker <this.tonybaker@gmail.com>
2019-03-26 00:44:14 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,10 @@
 class Dog
   def name=(dog_name) #Setter..Irish?
-    this_dogs_name = dog_name
+    @this_dogs_name = dog_name
   end
 
   def name  #Getter
-    this_dogs_name
+    @this_dogs_name
   end
 end
 
