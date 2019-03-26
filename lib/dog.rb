4164b0bce5d77e7d672898688d0d50225e39bf74
Tony Baker <this.tonybaker@gmail.com>
2019-03-26 00:56:42 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,13 @@
+class Dog
+  def name=(dog_name) #Setter..Irish?
+    @this_dogs_name = dog_name
+  end
+
+  def name  #Getter
+    @this_dogs_name
+  end
+end
+
+lassie = Dog.new
+lassie.name = "Lassie"
+lassie.name
